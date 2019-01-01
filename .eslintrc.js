4bf2ff7bd4741dd8705553553cd5a15661bbe700
Vladimir Kattsov <kattsoff@gmail.com>
2019-01-02 00:18:46 +0300

    Update eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,9 +10,9 @@ module.exports = {
     "graphql": false,
   },
   "parserOptions": {
+    "ecmaVersion": 2018,
     "sourceType": "module",
     "ecmaFeatures": {
-      "experimentalObjectRestSpread": true,
       "jsx": true,
     },
   }
